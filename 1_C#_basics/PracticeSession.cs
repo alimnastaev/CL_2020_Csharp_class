7c6ad01d43ac234b7a9460551b29bf7f4ff8a5c2
Alim Nastaev <a.nastaev@gmail.com>
2020-05-15 00:15:52 -0400

    PracticeSession.cs added: WIP- need to figure out how run both files independently since both of them have Main()

---
+++ 1_C#_basics/PracticeSession.cs
@@ -0,0 +1,26 @@
+using System;
+
+class PracticeSession
+{
+    static void Main()
+    {
+        // TODO Declare and assign a "thing" string variable.
+        // Example: "movie", "book", "color", etc.
+        string thing = "movie";
+
+        // TODO Prompt the user with the text "What is your name?" 
+        // and assign their value to a "name" string variable.
+        Console.Write("What is your name? ");
+        string name = Console.ReadLine();
+
+        // TODO Prompt the user with the text 
+        // "What is your favorite <thing>?" 
+        // and assign their value to a "favoriteThing" string variable.
+        Console.Write($"What is your favorite {thing}? ");
+        string movie = Console.ReadLine();
+
+        // TODO Write the user's name and favorite thing to the console.
+        // Example: "My name is James and my favorite movie is Toy Story."
+        Console.WriteLine($"My name is {name} and my favorite movie is {movie}.");
+    }
+}
